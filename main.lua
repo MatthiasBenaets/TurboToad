964a84e82a009f8bb2ddbee404dd28d9b6218ae3
MatthiasBenaets <matthias.benaets@gmail.com>
2025-05-25 10:09:02 +0200

    window sizing

--- main.lua
+++ main.lua
@@ -1,5 +1,28 @@
-function love.load() end
+local push = require("lib.push")
+local windowWidth, windowHeight = love.window.getDesktopDimensions()
+local virtualWidth, virtualHeight = 800, 600
+
+local menu = love.graphics.newImage("assets/menu.png")
+
+function love.load()
+	love.graphics.setDefaultFilter("nearest", "nearest")
+	push:setupScreen(
+		virtualWidth,
+		virtualHeight,
+		windowWidth,
+		windowHeight,
+		{ fullscreen = false, vsync = true, resizable = true }
+	)
+end
 
 function love.update(dt) end
 
-function love.draw() end
+function love.resize(w, h)
+	push:resize(w, h)
+end
+
+function love.draw()
+	push:start()
+	love.graphics.draw(menu, 0, 0)
+	push:finish()
+end
