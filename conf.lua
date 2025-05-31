dba7dae533a5e3e39300e0f61fbc2e7f1e9ffd08
MatthiasBenaets <matthias.benaets@gmail.com>
2025-05-31 12:02:33 +0200

    love2d init

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+	t.window.title = "TurboToad"
+	t.window.width = 1080
+	t.window.height = 720
+	t.window.fullscreen = false
+	t.window.resizable = false
+	t.window.vsync = true
+end
